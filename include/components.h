cf1b7ef72b5c5eee3ae5aaddfc6d6ea7d5ed7200
qiekn <qiekn@kimidayo.cn>
2025-05-06 13:03:26 +0800

    add: components

--- include/components.h
+++ include/components.h
@@ -1,11 +1,27 @@
 #pragma once
 
+#include <string>
 #include "types.h"
 
 // Properties
+
 struct Position : public Vector2Int {
   Position() : Vector2Int() {}
   Position(int x, int y) : Vector2Int(x, y) {}
+};
+
+enum class SpriteType {
+  kNoun,
+  kOperator,
+  kProperty,
+  kIcon,
+};
+
+struct SpriteRenderer {
+  SpriteType type;
+  std::string name;
+  size_t frames = 3;
+  bool is_animated = true;
 };
 
 // basic crates
