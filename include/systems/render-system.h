4d51e2c2bbebdac797a0056f0352f4b7faab5390
qiekn <qiekn@kimidayo.cn>
2025-05-06 13:00:41 +0800

    feat: render system (ecs)

---
+++ include/systems/render-system.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "managers/texture-manager.h"
+#include "types.h"
+
+class RenderSystem {
+public:
+  RenderSystem(Registry& registry, TextureManager& texture_manager)
+      : registry_(registry), texture_manager_(texture_manager) {}
+
+  virtual ~RenderSystem();
+
+  void DrawScene();
+  void DrawUI();
+
+private:
+  // scene
+  void DrawBackground();
+  void DrawGrid();
+  void DrawEntities();
+
+  // ui
+  void DrawImGui();
+
+  // helper functions
+
+private:
+  Registry& registry_;
+  TextureManager& texture_manager_;
+
+  /**
+   * @brief index for current texture2d (sprite animation)
+   */
+  int index_ = 0;
+  float timer_ = 0;
+  float duration_ = 0.2f;
+};
---
+++ src/systems/render-system.cpp
@@ -0,0 +1,72 @@
+#include "systems/render-system.h"
+#include <entt.h>
+#include <raylib.h>
+#include "components.h"
+#include "constants.h"
+#include "rlimgui.h"
+
+void RenderSystem::DrawScene() {
+  DrawBackground();
+  DrawGrid();
+  DrawEntities();
+}
+
+void RenderSystem::DrawUI() { DrawImGui(); }
+
+void RenderSystem::DrawBackground() {
+  auto background_outer = Color{38, 30, 67, 255};
+  auto background_inner = Color{26, 19, 35, 255};
+  ClearBackground(background_outer);
+  auto height = kCellSize * kRows;
+  auto width = kCellSize * kCols;
+  DrawRectangle(0, 0, width, height, background_inner);
+}
+
+void RenderSystem::DrawGrid() {
+  int scale = 1;
+  int sz = kCellSize * scale;
+  Color color = Color{47, 34, 86, 255};
+
+  // horizontal lines
+  for (int i = 0; i <= kRows; i++) {
+    DrawLine(0, i * sz, kCols * sz, i * sz, color);
+  }
+
+  // vertical lines
+  for (int i = 0; i <= kCols; i++) {
+    DrawLine(i * sz, 0, i * sz, kRows * sz, color);
+  }
+}
+
+void RenderSystem::DrawEntities() {
+  auto view = registry_.view<Position, SpriteRenderer>(entt::exclude<Hide>);
+  for (auto entity : view) {
+    const auto& pos = view.get<Position>(entity);
+    const auto& render = view.get<SpriteRenderer>(entity);
+    float px = pos.x * kCellSize;
+    float py = pos.y * kCellSize;
+
+    auto frames = texture_manager_.GetFrames(render.name);
+
+    // update current index of frame_texture
+    if (render.is_animated) {
+      timer_ += GetFrameTime();
+      if (timer_ >= duration_) {
+        timer_ -= duration_;
+        index_ = (index_ + 1) % frames.size();
+      }
+    }
+
+    Texture2D current_texture = frames[index_];
+    DrawTextureEx(current_texture, Vector2(px, py), 0, kScale, PINK);
+  }
+}
+
+void RenderSystem::DrawImGui() {
+  rlImGuiBegin();
+
+  bool open = true;
+  // ImGui::ShowDemoWindow(&open);
+
+  rlImGuiEnd();
+}
