a9e05913fc8fc41db779eb87c1ad98b60a97518f
qiekn <qiekn@kimidayo.cn>
2025-05-06 13:03:33 +0800

    update: constants

--- include/constants.h
+++ include/constants.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <filesystem>
 const int kScreenWidth = 1200;
 const int kScreenHeight = 960;
+
+const int KFps = 60;
 
 const bool kAnimated = false;
 
@@ -9,3 +12,6 @@ const int kCellSize = 24;
 const int kScale = 2;
 const int kRows = 18;
 const int kCols = 22;
+
+const std::filesystem::path kAssets = ASSETS;
+const std::filesystem::path kLevels = LEVELS;
