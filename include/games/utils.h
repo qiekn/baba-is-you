7d0522252ff443b41d7d5dd7068f32dbb8dc3fc3
qiekn <qiekn@kimidayo.cn>
2025-05-09 10:00:44 +0800

    feat: haxc

--- include/games/utils.h
+++ include/games/utils.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <raylib.h>
 #include <string>
 #include "games/object.h"
 
 // NONU_WALL -> text_wall
 // ICON_WALL -> wall
 std::string GetSpritePrefixName(ObjectType type);
+
+// convert ##3b250c -> raylib color
+Color Haxc(const std::string& color);
--- src/games/utils.cpp
+++ src/games/utils.cpp
@@ -1,4 +1,5 @@
 #include "games/utils.h"
+#include <raylib.h>
 #include "games/object.h"
 #include "magic-enum.h"
 #include "types.h"
@@ -12,4 +13,21 @@ std::string GetSpritePrefixName(ObjectType type) {
     name = ToLower(enum_name.data());
   }
   return name;
+}
+
+Color Haxc(const std::string& color) {
+  if (color.size() != 7 || color[0] != '#') {
+    TraceLog(LOG_ERROR, "Invalid color format: %s", color.c_str());
+    return WHITE;
+  }
+
+  try {
+    // Convert hex string (without #) to integer
+    unsigned int num = std::stoul(color.substr(1), nullptr, 16);
+    // Use Raylib's GetColor
+    return GetColor(num << 8 | 0xFF);  // Shift to RRGGBBAA format
+  } catch (const std::exception& e) {
+    TraceLog(LOG_ERROR, "Invalid hex color: %s", color.c_str());
+    return WHITE;
+  }
 }
