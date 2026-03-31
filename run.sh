4edca5e7052b73d3c1cfc74e6af74cf339cf134f
qiekn <qiekndev@gmail.com>
2026-03-31 23:23:06 +0800

    chore: update run.sh (using cmake and ninja)

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Config
+PROJECT_NAME="game"
+
+BUILD_DIR="build"
+
+DEBUGGER="gdb -q"
+
+# Ensure build dir exists
+if [ ! -d "${BUILD_DIR}" ]; then
+  echo "[INFO] ${BUILD_DIR} not found, mkdir ${BUILD_DIR}and running cmake..."
+  cmake -S . -B "${BUILD_DIR}" -G Ninja || exit 1
+fi
+
+if [ "$1" = "debug" ]; then
+  ${DEBUGGER} "./${BUILD_DIR}/${PROJECT_NAME}"
+else
+  cmake --build ${BUILD_DIR} -j  && "./${BUILD_DIR}/${PROJECT_NAME}"
+fi
+
+# vim: ft=sh ts=2 sw=2 et
