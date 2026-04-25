8a2e9ac6106031797d1bd29db7025c1136d48a37
qiekn <qiekndev@gmail.com>
2026-04-26 07:17:04 +0800

    qiekn(sprite_sheet): add comment to explain baba-is-you SpriteSheet class

--- src/sprite_sheet.h
+++ src/sprite_sheet.h
@@ -7,6 +7,21 @@
 
 #include "ids.h"
 
+/**
+ * @class SpriteSheet
+ * @brief Hello, this is qiekn. Let me explain this class
+ *
+ * In this game baba is you, we have different sprites
+ * for more, you can see: [Game Elements](https://www.spriters-resource.com/pc_computer/babaisyou/)
+ *
+ * 1. Tiled                - this is the map layer like grass, wall, cloud, fence
+ * 2. Text                 - just a 3 frame animation
+ * 3. Object (Static)      - just a 3 frame animation
+ * 4. Object (Directional) - you can filter arrow_*.png to understand this
+ * 5. Object (Character)   - the most complex, e.g. baba_xx_y.png
+ *    xx is [0, 31] -> 8 * 4 = 32, [0, 7] is right, the next 8 is up, and then left, down
+ *    y  is [1, 3] -> when we don't move, the sprites has a 3 frame animation
+ */
 class SpriteSheet {
  public:
   static constexpr int kFrameCount = 3;
